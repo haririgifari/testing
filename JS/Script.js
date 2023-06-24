060d6c617154c6fcc1bc59245c092bc644e26859
haririgifari <haririgifari14@gmail.com>
2023-06-24 16:03:02 +0700

    penambahan folder

---
+++ JS/Script.js
@@ -0,0 +1,10 @@
+/*var nav = document.querySelector('nav');
+document.addEventListener('scroll', function () {
+  // console.log(Hello);
+  if (window.scrollY > 100) {
+    nav.style.backgroundColor = '#DCDCDC';
+    nav.style.transition = '2s';
+  } else {
+    nav.style.backgroundColor = 'transparent';
+  }
+});**/
